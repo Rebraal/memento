b7fe5d0e600eff4a67e04f495b01512200b564f8
Rebraal <57767529+Rebraal@users.noreply.github.com>
2020-03-31 15:17:26 +0100

    Update DATE.js

--- DATE.js
+++ DATE.js
@@ -1,5 +1,10 @@
 //a custom made DATE object that parses Memento entries into strings. I intend to use this in most Memento scripts where necessary.
-function DATE (entry){
+/*2020-03-31
+1515
+added string input method.
+*/
+function DATE (input){
+	
 	//this assumes (briefly tested) that "2020-03-31 10:40" > "2020-03-31 10:39" 
 	//as of 2020-03-31 with phone date set to both pre and post DST, all Time.getHours() values are 1 hour behind where they should be.
 	this.correctHour = function(h){
@@ -10,13 +15,23 @@ function DATE (entry){
 	this.dd = function(n){
 		return n < 10 ? "0" + n : "" + n;
 	};
-		
-	this.year 	= entry.field("Date").getFullYear();
-	this.month 	= entry.field("Date").getMonth()+1;
-	this.day 	= entry.field("Date").getDate();
-	this.hour 	= this.correctHour(entry.field("Time").getHours());
-	this.minute = entry.field("Time").getMinutes();
-	
+	//if entry object given as string
+	if(typeof input == "string"){
+		var m = input.match(/(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2})/);
+		if(m != null){
+			this.year 	= m[1];
+			this.month 	= m[2];
+			this.day 	= m[3];
+			this.hour 	= m[4];
+			this.minute	= m[5];
+		}
+	} else {
+		this.year 	= input.field("Date").getFullYear();
+		this.month 	= input.field("Date").getMonth()+1;
+		this.day 	= input.field("Date").getDate();
+		this.hour 	= this.correctHour(input.field("Time").getHours());
+		this.minute = input.field("Time").getMinutes();
+	}
 	this.dateStamp = 	this.year				+ "-" + 
 						this.dd(this.month) 	+ "-" + 
 						this.dd(this.day )		+ " " + 
