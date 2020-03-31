2a60c4c5a8903f6c86aba5d7c46d225c115c9d41
Rebraal <57767529+Rebraal@users.noreply.github.com>
2020-03-31 17:15:38 +0100

    Update DATE.js

--- DATE.js
+++ DATE.js
@@ -1,10 +1,14 @@
 //a custom made DATE object that parses Memento entries into strings. I intend to use this in most Memento scripts where necessary.
 /*2020-03-31
 1515
-Added string input method.
-1700
-Corrected dayStart not stepping back a day for hours < 4 
+added string input method.
+1715
+corrected not stepping back a day for dayStart if hour < 4.
+this causes problems at the start of a month... so we cheat and use the Date object for a bit.
 */
+var version = "DATE v2020-03-31 1715";
+log != undefined ? log(version) : console.log(version);
+
 function DATE (input){
 	
 	//this assumes (briefly tested) that "2020-03-31 10:40" > "2020-03-31 10:39" 
@@ -17,6 +21,7 @@ function DATE (input){
 	this.dd = function(n){
 		return n < 10 ? "0" + n : "" + n;
 	};
+
 	//if entry object given as string
 	if(typeof input == "string"){
 		var m = input.match(/(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2})/);
@@ -34,17 +39,31 @@ function DATE (input){
 		this.hour 	= this.correctHour(input.field("Time").getHours());
 		this.minute = input.field("Time").getMinutes();
 	}
+	
 	this.dateStamp = 	this.year				+ "-" + 
 						this.dd(this.month) 	+ "-" + 
 						this.dd(this.day )		+ " " + 
 						this.dd(this.hour) 		+ ":" + 
 						this.dd(this.minute);
-				
-	//04:00 defined as the start of the day.
-	this.dayStart = this.year	+ "-" + 
-					this.dd(this.month) + "-" + 
-					this.dd(this.day )	+ " " +
-					"04:00";
+						
+	this.DATEtoDate = function(){
+		return new Date(this.dateStamp).getTime();
+	};			
+	
+	//04:00 defined as the start of the day, so if time is 00:00 < time < 03:39, step back a day
+	if(this.hour < 4){
+		//step back 12 hours - even accounting for DST this should be in the middle of the previous day
+		var t = new Date(DATEtoDate() - 12*60*60*1000);
+		this.dayStart = t.getFullYear() 		+ "-" +
+						this.dd(t.getMonth()+1)	+ "-" +
+						this.dd(t.getDate())	+ " " +
+						"04:00";
+	} else {
+		this.dayStart = this.year			+ "-" + 
+						this.dd(this.month) + "-" + 
+						this.dd(this.day )	+ " " +
+						"04:00";
+	}
 					
 	this.date = this.year			+ "-" + 
 				this.dd(this.month) + "-" + 
@@ -52,4 +71,5 @@ function DATE (input){
 	
 	this.time = this.dd(this.hour) 		+ ":" + 
 				this.dd(this.minute);
+	
 }
