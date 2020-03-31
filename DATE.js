108c709339ee83fa6c7755d5cad6a038cdc35bb5
Rebraal <57767529+Rebraal@users.noreply.github.com>
2020-03-31 18:03:41 +0100

    Update DATE.js

--- DATE.js
+++ DATE.js
@@ -5,22 +5,59 @@ added string input method.
 1715
 corrected not stepping back a day for dayStart if hour < 4.
 this causes problems at the start of a month... so we cheat and use the Date object for a bit.
+1800
+added in stepBackDate, split out subroutines to avoid repeated definitions.
 */
-var version = "DATE v2020-03-31 1715";
-log != undefined ? log(version) : console.log(version);
+var version = "DATE v2020-03-31 1800";
+typeof log != undefined ? log(version) : console.log(version);
+
+function correctHour(h){
+	h += 1;
+	return  h > 23 ? 0 : h;
+}
+//returns a double digit string from a number - ie 1 => "01"
+function dd(n){
+	return n < 10 ? "0" + n : "" + n;
+}
+//returns the previous start of day
+function stepBackDate(y, m, d){
+	d--;
+	if(d < 1){
+		m--;
+		if(m < 1){
+			y--;
+			m = 12;
+			d = 31;
+		} else {
+			d = daysInMonth(m);
+		}
+	}
+	return y + "-" + dd(m) + "-" + dd(d) + " 04:00";
+}
+
+function isLeapYear(y){
+	return (y % 100 === 0) ? (y % 400 === 0) : (y % 4 === 0);
+}
+
+//return days in the month, Jan = 1
+function daysInMonth(y, m){
+	switch(m){
+		case 11:
+		case 9:
+		case 6:
+		case 4:
+			return 30;
+		case 2:
+			return isLeapYear(y) ? 29 : 28;
+		default:
+			return 31;
+	}
+}
 
 function DATE (input){
 	
 	//this assumes (briefly tested) that "2020-03-31 10:40" > "2020-03-31 10:39" 
 	//as of 2020-03-31 with phone date set to both pre and post DST, all Time.getHours() values are 1 hour behind where they should be.
-	this.correctHour = function(h){
-		h += 1;
-		return  h > 23 ? 0 : h;
-	};
-	//returns a double digit string from a number - ie 1 => "01"
-	this.dd = function(n){
-		return n < 10 ? "0" + n : "" + n;
-	};
 
 	//if entry object given as string
 	if(typeof input == "string"){
@@ -36,40 +73,31 @@ function DATE (input){
 		this.year 	= input.field("Date").getFullYear();
 		this.month 	= input.field("Date").getMonth()+1;
 		this.day 	= input.field("Date").getDate();
-		this.hour 	= this.correctHour(input.field("Time").getHours());
+		this.hour 	= correctHour(input.field("Time").getHours());
 		this.minute = input.field("Time").getMinutes();
 	}
 	
-	this.dateStamp = 	this.year				+ "-" + 
-						this.dd(this.month) 	+ "-" + 
-						this.dd(this.day )		+ " " + 
-						this.dd(this.hour) 		+ ":" + 
-						this.dd(this.minute);
-						
-	this.DATEtoDate = function(){
-		return new Date(this.dateStamp).getTime();
-	};			
-	
+	this.dateStamp = 	this.year		+ "-" + 
+						dd(this.month) 	+ "-" + 
+						dd(this.day )	+ " " + 
+						dd(this.hour) 	+ ":" + 
+						dd(this.minute);
+
 	//04:00 defined as the start of the day, so if time is 00:00 < time < 03:39, step back a day
 	if(this.hour < 4){
-		//step back 12 hours - even accounting for DST this should be in the middle of the previous day
-		var t = new Date(this.DATEtoDate() - 12*60*60*1000);
-		this.dayStart = t.getFullYear() 		+ "-" +
-						this.dd(t.getMonth()+1)	+ "-" +
-						this.dd(t.getDate())	+ " " +
-						"04:00";
+		this.dayStart = stepBackDate(this.year, this.month, this.day);
 	} else {
-		this.dayStart = this.year			+ "-" + 
-						this.dd(this.month) + "-" + 
-						this.dd(this.day )	+ " " +
+		this.dayStart = this.year		+ "-" + 
+						dd(this.month) 	+ "-" + 
+						dd(this.day )	+ " " +
 						"04:00";
 	}
 					
-	this.date = this.year			+ "-" + 
-				this.dd(this.month) + "-" + 
-				this.dd(this.day )	+ " ";
+	this.date = this.year		+ "-" + 
+				dd(this.month) 	+ "-" + 
+				dd(this.day )	+ " ";
 	
-	this.time = this.dd(this.hour) 		+ ":" + 
-				this.dd(this.minute);
+	this.time = dd(this.hour) 	+ ":" + 
+				dd(this.minute);
 	
 }
